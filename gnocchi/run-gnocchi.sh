b49ab7d1dd82b6a450db9ae1248fbd163f1ca981
Fabian Zimmermann <dev.faz@googlemail.com>
2019-03-11 08:49:31 +0100

    gnocchi/run-gnocchi.sh: use crudini to configure uwsgi mode and proxy_headers_parsing

--- gnocchi/run-gnocchi.sh
+++ gnocchi/run-gnocchi.sh
@@ -2,6 +2,10 @@
 printf "[indexer]\nurl = postgresql://${POSTGRESQL_USER}:${POSTGRESQL_PASSWORD}@indexer/postgres\n" > /etc/gnocchi/gnocchi.conf
 printf "[storage]\ncoordination_url = redis://:${REDIS_PASSWORD}@storage\n" >> /etc/gnocchi/gnocchi.conf
 printf "[incoming]\ndriver = redis\nredis_url = redis://:${REDIS_PASSWORD}@storage\n" >> /etc/gnocchi/gnocchi.conf
+crudini --set /etc/gnocchi/gnocchi.conf api uwsgi_mode http-socket
+crudini --set /etc/gnocchi/gnocchi.conf api enable_proxy_headers_parsing true
+#crudini --set /etc/gnocchi/gnocchi.conf DEFAULT debug true
+#crudini --set /etc/gnocchi/gnocchi.conf metricd greedy false
 
 # NOTE(sileht): To get Gnocchi version build log
 pip freeze | grep gnocchi
